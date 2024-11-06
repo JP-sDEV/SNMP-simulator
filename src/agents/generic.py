48defe881b17330eaf3f20abd2156cd832134b96
JP-sDEV <jon.pablo80@gmail.com>
2024-11-06 15:06:32 -0500

    docs: add docstring

--- src/agents/generic.py
+++ src/agents/generic.py
@@ -12,11 +12,55 @@ load_dotenv(env_file)  # Load the appropriate environment file
 
 
 class SNMPNotification:
+    """
+    Represents an SNMP notificaiton for sending a trap message.
+
+    This class allows the construction of an SNMP notification with dynamic
+    varbinds (key-value pairs) representing different attributes of a
+    transaction or status update. It is useful for sending SNMP trap messages
+    with specific OIDs and associated values.
+
+    Attributes:
+        notification_type_OID (str): The OID for the notification type.
+        varbinds (dict [str:str]): Key-value pairs where each OID (str) is
+            associated with a value (str) to be sent in the notification
+    """
     def __init__(self, notification_type_OID, varbinds):
+        """
+        Initializes a SNMPNotification instance with the provided notification
+        type OID and a dictionary of varbinds.
+
+        Args:
+            notification_type_OID (str): The object identifier for the type of
+            notification.
+            varbinds (dict): Key-value pairs where each key is an OID (str)
+                and each value is the data (str) to be sent in the
+                notification.
+
+        Example:
+            varbinds = {
+                '1.3.6.1.4.1.9.9.599.1.3.1': 'status',
+                '1.3.6.1.4.1.9.9.599.1.3.2': 'success'
+            }
+            notification = SNMPNotification('1.3.6.1.4.1.9.9.599.1.1',
+                                            varbinds)
+
+        """
         self.notification_type_OID = notification_type_OID
         self.varbinds = varbinds
 
     def create(self):
+        """
+        Creates an SNMP NotificationType object based on the provided OID and
+        varbinds.
+
+        Iterates over the varbinds dictionary to add each OID-value pair to the
+        NotificationType object as a varbind.
+
+        Returns:
+            NotificationType: A configured SNMP notification with all varbinds
+            added.
+        """
         # Create the NotificationType object
         notification = NotificationType(ObjectIdentity(
             self.notification_type_OID))
@@ -33,11 +77,36 @@ class SNMPNotification:
 
 
 class SNMPAgent:
+    """
+    A class to represent an SNMP agent that constructs and sends SNMP traps
+    (notifications) with customizable varbinds and notification types.
+
+    Attributes:
+        snmp_engine (SnmpEngine): The SNMP engine for sending traps.
+        community (CommunityData): SNMP community data using SNMPv2c.
+        context (ContextData): SNMP context data.
+        target (dict): Target IP and port for the SNMP trap.
+        notification_OID (str): The Object Identifier (OID) for the
+        notification type.
+        varbinds (dict): Dictionary of OIDs and their associated values.
+    """
     def __init__(self,
                  ipv4_host=os.getenv('IPv4_HOST_IP'),
                  port=os.getenv('PORT'),
                  notification_OID=None,
                  varbinds=None):
+        """
+        Initializes the SNMPAgent with host details, notification OID, and
+        varbinds.
+
+        Args:
+            ipv4_host (str): IP address of the SNMP target.
+            port (int): Port number of the SNMP target.
+            notification_OID (str, optional): OID for the type of SNMP
+                notification.
+            varbinds (dict, optional): Dictionary of varbinds where each key
+                is an OID (str) and value is the data associated with it.
+        """
 
         self.snmp_engine = SnmpEngine()
         self.community = CommunityData('public', mpModel=1)  # SNMPv2c
@@ -50,32 +119,78 @@ class SNMPAgent:
         self.notification_OID = notification_OID
         self.varbinds = varbinds
 
-    # varbinds
+    # Varbinds
     def set_notifcation_type(self, OID):
+        """
+        Sets the OID for the SNMP notification type.
+
+        Args:
+            OID (str): The Object Identifier (OID) for the notification type.
+        """
         self.notification_OID = OID
 
     def add_varbind(self, OID, value):
-        if OID not in self.varbinds:
-            # raise error
-            pass
+        """
+        Adds a new varbind to the agent's varbind dictionary.
+
+        Args:
+            OID (str): The Object Identifier (OID) for the varbind.
+            value (str): The value to be associated with the OID.
+
+        Raises:
+            KeyError: If the OID already exists in the varbinds dictionary.
+        """
+        if OID in self.varbinds:
+            raise KeyError(f"OID '{OID}' already exists in varbinds. Please\
+                           ensure OID is not already in use.")
         else:
             self.varbinds[OID] = value
 
     def remove_varbind(self, OID):
+        """
+        Removes a varbind from the agent's varbind dictionary.
+
+        Args:
+            OID (str): The Object Identifier (OID) to remove from varbinds.
+
+        Raises:
+            KeyError: If the OID is not found in the varbinds dictionary.
+        """
         if OID not in self.varbinds:
-            # raise error
-            pass
+            raise KeyError(f"OID '{OID}' not found in varbinds. Please ensure\
+                           it exists before removing.")
         else:
             del self.varbinds[OID]
 
     def edit_varbind(self, OID, value):
+        """
+        Edits the value of an existing varbind in the agent's varbind
+        dictionary.
+
+        Args:
+            OID (str): The Object Identifier (OID) of the varbind to edit.
+            value (str): The new value to associate with the OID.
+
+        Raises:
+            KeyError: If the OID is not found in the varbinds dictionary.
+        """
         if OID not in self.varbinds:
-            # raise error
-            pass
+            raise KeyError(f"OID '{OID}' not found in varbinds. Please ensure\
+                           it exists before editing.")
         else:
             self.varbinds[OID] = value
 
     async def send_trap(self):
+        """
+        Constructs and sends an SNMP trap (notification) using the specified
+        varbinds and notification type.
+
+        Uses `SNMPNotification` to create the notification object, then sends
+        it to the target IP and port.
+
+        Raises:
+            Exception: If the trap cannot be sent due to connection issues.
+        """
         # Set up SNMP target
         target = await UdpTransportTarget.create((self.target['ip'],
                                                   int(self.target['port'])))
--- src/commands/trap/manager.py
+++ src/commands/trap/manager.py
@@ -13,44 +13,75 @@ load_dotenv(env_file)  # Load the appropriate environment file
 
 
 class SNMPManager:
+    """
+    A Simple Network Management Protocol (SNMP) manager that listens for SNMP
+    notifications on both IPv4 and IPv6, and processes incoming SNMP messages
+    asynchronously using the asyncio library.
+
+    Attributes:
+        ipv4_host (str): The IPv4 address on which the SNMP manager listens.
+        ipv6_host (str): The IPv6 address on which the SNMP manager listens.
+        port (int): The port on which the SNMP manager listens for
+            notifications.
+        transportDispatcher (AsyncioDispatcher): Manages the transport
+            mechanism for SNMP messages.
+        _running (bool): A flag indicating whether the manager is actively
+            running.
+    """
+
     def __init__(self, ipv4_host=os.getenv('IPv4_HOST_IP'),
                  ipv6_host=os.getenv('IPv6_HOST_IP'),
                  port=os.getenv('PORT')):
+        """
+        Initializes the SNMPManager instance, setting up the dispatcher to
+        handle incoming SNMP messages on both IPv4 and IPv6.
 
+        Args:
+            ipv4_host (str): The IPv4 address to bind to.
+            ipv6_host (str): The IPv6 address to bind to.
+            port (int): The port to bind to for receiving SNMP messages.
+        """
         self.transportDispatcher = AsyncioDispatcher()
         self.transportDispatcher.register_recv_callback(self._callback)
         self.ipv4_host = ipv4_host
         self.ipv6_host = ipv6_host
-        self.port = port
+        self.port = int(port)
 
-        # UDP/IPv4 setup
+        # Set up transport mechanisms for IPv4 and IPv6
         self.transportDispatcher.register_transport(
-            udp.DOMAIN_NAME, udp.UdpAsyncioTransport().open_server_mode(
-                (self.ipv4_host, self.port)
-            )
+            udp.DOMAIN_NAME,
+            udp.UdpAsyncioTransport().open_server_mode((self.ipv4_host,
+                                                        self.port))
         )
-
-        # UDP/IPv6 setup
         self.transportDispatcher.register_transport(
-            udp6.DOMAIN_NAME, udp6.Udp6AsyncioTransport().open_server_mode(
-               (self.ipv6_host, self.port)
-            )
+            udp6.DOMAIN_NAME,
+            udp6.Udp6AsyncioTransport().open_server_mode((self.ipv6_host,
+                                                          self.port))
         )
 
         self.transportDispatcher.job_started(1)
         self._running = True
 
-        # Register signal handlers
-        # Handle Ctrl-C
+        # Register signal handlers for graceful shutdown
         signal.signal(signal.SIGINT, self.signal_handler)
-        # Handle termination
         signal.signal(signal.SIGTERM, self.signal_handler)
 
     def signal_handler(self, signum, frame):
+        """
+        Handles termination signals to gracefully stop the SNMP manager.
+
+        Args:
+            signum (int): Signal number.
+            frame (FrameType): Current stack frame.
+        """
         print("Signal received, shutting down...")
         self.stop()
 
     async def run_async(self):
+        """
+        Asynchronously runs the SNMP manager's dispatcher in a loop, allowing
+        it to process incoming SNMP messages until stopped.
+        """
         print("Started SNMP Manager. Press Ctrl-C to stop.")
         while self._running:
             self.transportDispatcher.run_dispatcher()
@@ -58,6 +89,20 @@ class SNMPManager:
 
     def _callback(self, transportDispatcher, transportDomain, transportAddress,
                   wholeMsg):
+        """
+        Callback function to process received SNMP messages.
+
+        Args:
+            transportDispatcher (AsyncioDispatcher): The dispatcher handling 
+                the transport.
+            transportDomain (tuple): The transport domain (protocol) used
+                (e.g., UDP).
+            transportAddress (tuple): Address tuple of the sender.
+            wholeMsg (bytes): The raw message received.
+
+        Returns:
+            bytes: Remaining message content after processing.
+        """
         while wholeMsg:
             msgVer = int(api.decodeMessageVersion(wholeMsg))
             if msgVer in api.PROTOCOL_MODULES:
@@ -70,8 +115,8 @@ class SNMPManager:
                 wholeMsg,
                 asn1Spec=pMod.Message(),
             )
-            print("Notification message from {}:{}: ".format(transportDomain,
-                                                             transportAddress))
+            print("Notification message from {}:{}: ".format(
+                transportDomain, transportAddress))
             reqPDU = pMod.apiMessage.get_pdu(reqMsg)
             if reqPDU.isSameTypeWith(pMod.TrapPDU()):
                 varBinds = pMod.apiPDU.get_varbinds(reqPDU)
@@ -81,6 +126,11 @@ class SNMPManager:
         return wholeMsg
 
     async def run(self):
+        """
+        Starts the SNMP manager and runs it asynchronously until interrupted.
+
+        Catches KeyboardInterrupt to handle graceful shutdown.
+        """
         try:
             await self.run_async()
         except KeyboardInterrupt:
@@ -89,11 +139,18 @@ class SNMPManager:
             self.transportDispatcher.close_dispatcher()
 
     def stop(self):
+        """
+        Stops the SNMP manager, halting the dispatcher and setting `_running`
+        to False.
+        """
         self._running = False
         self.transportDispatcher.close_dispatcher()
 
 
 async def start_manager():
+    """
+    Creates and starts an instance of SNMPManager asynchronously.
+    """
     manager = SNMPManager()
     await manager.run()
 
